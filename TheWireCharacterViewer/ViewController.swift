2c145cb227e996ed6d392574eec0fd36d4a69962
fmartin0212 <fmartin0212@gmail.com>
2019-11-20 19:40:39 -0600

    Adds two additional targets

---
+++ TheWireCharacterViewer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TheWireCharacterViewer
+//
+//  Created by Frank Martin Jr on 11/20/19.
+//  Copyright © 2019 FrankMartin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
